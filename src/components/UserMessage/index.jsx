dd89cf06b1b0a32d9e73b6d5b89d9c2feaee8a17
petark7 <kuzmanovski.petar@uklo.edu.mk>
2023-07-17 14:43:21 +0200

    Fix alignment
    For some  reason, previous alignment didn't get saved

--- src/components/UserMessage/index.jsx
+++ src/components/UserMessage/index.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 const UserMessage = ({ sender, message, image }) => {
-	const position = sender ? 'end' : 'start';
+	const position = sender ? 'chat-end' : 'chat-start';
 	const color = sender ? 'bg-blue-900' : 'bg-gray-700';
 
 	return (
-		<div className={`chat chat-${position}`}>
+		<div className={`chat ${position}`}>
 			<div className="chat-image avatar">
 				<div className="w-10 rounded-full">
 					<img src={image} />
